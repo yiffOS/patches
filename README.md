15b774e364fd665a2f52e4511b4b290da8a293a5
Evie Viau <evie@uwueviee.live>
2021-10-31 12:50:57 +0000

    Add automated script for packaging the kernel

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Various patches for yiffOS packages.
 ### linux:   
 * linux/good_panic_message - Changes the kernel panic message to be better   
 * linux/config - Kernel compile config - Partly from [Arch Linux](https://archlinux.org/)
+* linux/package-kernel.sh - Kernel packaging script
 
 ### glibc:   
 * glibc/fhs-runtime - Patches GLibc for FHS runtime directory compilance - From [Linux From Scratch](https://www.linuxfromscratch.org/)
---
+++ linux/package-kernel.sh
@@ -0,0 +1,102 @@
+MAJOR_VERSION=5
+VERSION=5.14.15
+yiffOS_VERSION=0.1.1
+
+echo "Building for Linux Kernel v${VERSION} for yiffOS ${yiffOS_VERSION} with makeflags ${1}"
+
+# Download source and patches
+curl -LO https://cdn.kernel.org/pub/linux/kernel/v$MAJOR_VERSION.x/linux-$VERSION.tar.xz
+curl -LO https://gitlab.com/yiffos/core/patches/-/raw/main/linux/good_panic_message.patch
+curl -LO https://gitlab.com/yiffos/core/patches/-/raw/main/linux/config
+
+# Make directories
+mkdir -p packages/linux-$VERSION/data/boot
+mkdir -p packages/linux-headers-$VERSION/data/
+
+# Extract source for headers
+tar xvf linux-$VERSION.tar.xz
+cd linux-$VERSION
+
+# Clear source
+make mrproper
+
+# Make headers
+make headers
+
+# Delete unneeded files
+find usr/include -name '.*' -delete
+rm usr/include/Makefile
+
+# Package headers
+cp -rv usr/include ../packages/linux-headers-$VERSION/data/usr
+
+# Clear source
+cd ../
+rm -r linux-$VERSION
+
+# Extract source for kernel
+tar xvf linux-$VERSION.tar.xz
+cd linux-$VERSION
+
+# Clear source
+make mrproper
+
+# Apply patches and copy config 
+patch -Np1 -i ../good_panic_message.patch
+cp ../config .config
+
+# Make the kernel and modules
+make $1
+make INSTALL_MOD_PATH=../packages/linux-$VERSION/data/usr INSTALL_MOD_STRIP=1 modules_install
+
+# Package kernel and mopdules
+cp -iv arch/x86/boot/bzImage ../packages/linux-$VERSION/data/boot/vmlinuz-$VERSION-yiffOS-$yiffOS_VERSION
+cp -iv System.map ../packages/linux-$VERSION/data/boot/System.map-$VERSION
+install -d ../packages/linux-$VERSION/data/usr/share/doc/linux-$VERSION
+cp -r Documentation/* ../packages/linux-$VERSION/data/usr/share/doc/linux-$VERSION
+
+# Go back to root
+cd ..
+
+# Create PKG files
+cat << EOL >> ./packages/linux-${VERSION}/PKG
+{
+	"name": "linux",
+	"version": "${VERSION}",
+	"epoch": 0,
+	"description":"The Linux kernel and modules.",
+	"groups": "",
+	"url": "https://www.kernel.org/",
+	"license": "GPL2",
+	"depends": "",
+	"optional_depends: "",
+	"provides": "linux",
+	"conflicts": "",
+	"replaces": "",
+	"sha512sum": ""
+}
+EOL
+
+cat << EOL >> ./packages/linux-headers-${VERSION}/PKG
+{
+	"name": "linux-headers",
+	"version": "${VERSION}",    
+	"epoch": 0,
+	"description":"Headers and scripts for building modules for the Linux kernel.",
+	"groups": "",
+	"url": "https://www.kernel.org/",
+	"license": "GPL2",
+	"depends": "",
+	"optional_depends: "",
+	"provides": "linux-headers",
+	"conflicts": "",
+	"replaces": "",
+	"sha512sum": ""
+}
+EOL
+
+# Compress packages
+tar cvf linux-${VERSION}.tar ./packages/linux-${VERSION}/PKG ./packages/linux-${VERSION}/data
+tar cvf linux-headers-${VERSION}.tar ./packages/linux-headers-${VERSION}/PKG ./packages/linux-headers-${VERSION}/data
+xz linux-${VERSION}.tar
+xz linux-headers-${VERSION}.tar
